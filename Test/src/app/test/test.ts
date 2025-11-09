ce56bc67d156c7da630797adb72374c05dbf43c9
dinos <dinostupar78@gmail.com>
2025-11-09 23:01:00 +0100

    commit

--- Test/src/app/test/test.ts
+++ Test/src/app/test/test.ts
@@ -1,11 +1,65 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-test',
-  imports: [],
+  imports: [
+    FormsModule,
+    DatePipe,
+    CommonModule
+  ],
   templateUrl: './test.html',
   styleUrl: './test.css',
 })
 export class Test {
+
+  posts = [
+    { user: "Dino", timestamp: new Date(), comment: "Ovo je prvi komentar", editing: false },
+    { user: "Jakov", timestamp: new Date(), comment: "Ovo je drugi komentar", editing: false }
+  ];
+
+  adding = false;
+  newUser = '';
+  newComment = '';
+  originalComment = '';
+
+
+  toggleAdd() {
+    this.adding = !this.adding;
+  }
+
+  addPost() {
+    if (this.newUser.trim() !== '' && this.newComment.trim() !== '') {
+      this.posts.push({
+        user: this.newUser,
+        timestamp: new Date(),
+        comment: this.newComment,
+        editing: false
+      });
+      this.newUser = '';
+      this.newComment = '';
+      this.adding = false;
+    }
+  }
+
+  deletePost(index: number) {
+    this.posts.splice(index, 1);
+  }
+
+  editPost(p: any) {
+    p.editing = true;
+    this.originalComment = p.comment;
+  }
+
+  savePost(p: any) {
+    p.editing = false;
+  }
+
+  cancelEdit(p: any){
+    p.comment = this.originalComment;
+    p.editing = false;
+  }
 
 }
